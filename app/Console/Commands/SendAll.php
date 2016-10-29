c811e86d2f348a078eeb034b2003dba6e0b6fb09
Roberto Guido <bob4job@gmail.com>
2016-10-29 17:53:41 +0000

    correzioni minori in comando invio arbitrario

--- app/Console/Commands/SendAll.php
+++ app/Console/Commands/SendAll.php
@@ -25,31 +25,31 @@ class SendAll extends Command
     {
         $users_file = $this->argument('users');
         $text_file = $this->argument('message');
-        
+
         if (file_exists($users_file) == false) {
             echo "Impossibile aprire il file con le mail degli utenti coinvolti.\n";
             exit();
         }
-        
+
         if (file_exists($text_file) == false) {
             echo "Impossibile aprire il file con il testo della mail di accompagnamento.\n";
             exit();
         }
-        
+
         $text = file_get_contents($text_file);
         $mails = file($users_file);
-        
+
         foreach($mails as $m) {
             $m = strtolower(trim($m));
+            if (empty($m))
+                continue;
+
             $user = User::where(DB::raw('LOWER(email)'), '=', $m)->first();
 
             if ($user != null) {
                 $files = Cloud::getContents($user->username);
-                if (empty($files))
-                    continue;
-
                 $local_files = [];
-                
+
                 foreach($files as $f) {
                     $filename = basename($f);
                     $path = sprintf('/tmp/%s', $filename);
@@ -57,7 +57,7 @@ class SendAll extends Command
                     file_put_contents($path, $data);
                     $local_files[] = $path;
                 }
-                
+
                 if (empty($local_files)) {
                     echo "Nessun file da spedire a " . $m . "\n";
                     continue;
@@ -67,24 +67,26 @@ class SendAll extends Command
                     try {
                         Mail::send('emails.empty', ['text' => $user->group->mailtext], function ($m) use ($user, $local_files, $e) {
                             $m->to($e, $user->name . ' ' . $user->surname)->subject('nuovo documento disponibile');
-                            
+
                             foreach($local_files as $filepath)
                                 $m->attach($filepath);
                         });
-                        
+
                         echo $m . " - " . $e . ": OK\n";
                     }
                     catch (\Exception $ex) {
                         echo $m . " - " . $e . ": FAILED\n";
                     }
                 }
-                
+
                 foreach($local_files as $f)
                     unlink($f);
             }
             else {
                 echo "Utente non trovato per mail " . $m . "\n";
             }
+
+            usleep(500000);
         }
     }
 }
