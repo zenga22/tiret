24e9d03b7af94f08fb03294bad447594298e5573
Roberto Guido <bob4job@gmail.com>
2017-01-18 17:14:01 +0100

    parametro per raggruppamento files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,15 +27,59 @@
             @if(count($files) == 0)
                 <p class="alert alert-info">Non hai files assegnati</p>
             @else
-                <table class="table filelist filteratable">
-                    <tbody>
-                        @foreach($files as $file)
-                            <tr>
-                                <td><a href="{{ url('file/' . $file) }}">{{ basename($file) }}</a></td>
-                            </tr>
+                @if(env('GROUPING_RULE', '') != '')
+                    <?php
+
+                        $file_groups = [];
+
+                        foreach($files as $file) {
+                            preg_match(env('GROUPING_RULE'), basename($file), $matches);
+                            if (isset($matches['key']))
+                                $name = $matches['key'];
+                            else
+                                $name = '?';
+
+                            if (isset($file_groups[$name]) == false)
+                                $file_groups[$name] = [];
+                            $file_groups[$name][] = $file;
+                        }
+
+                    ?>
+
+                    <ul class="nav nav-tabs" role="tablist">
+                        <?php $index = 0 ?>
+                        @foreach($file_groups as $name => $files)
+                            <li role="presentation" {!! $index++ == 0 ? 'class="active"' : '' !!}><a href="#{{ $name }}" aria-controls="{{ $name }}" role="tab" data-toggle="tab">{{ $name }}</a></li>
                         @endforeach
-                    </tbody>
-                </table>
+                    </ul>
+
+                    <div class="tab-content">
+                        <?php $index = 0 ?>
+                        @foreach($file_groups as $name => $files)
+                            <div role="tabpanel" class="tab-pane {{ $index++ == 0 ? 'active' : '' }}" id="{{ $name }}">
+                                <table class="table filelist filteratable">
+                                    <tbody>
+                                        @foreach($files as $file)
+                                            <tr>
+                                                <td><a href="{{ url('file/' . $file) }}">{{ basename($file) }}</a></td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        @endforeach
+                    </div>
+                @else
+                    <table class="table filelist filteratable">
+                        <tbody>
+                            @foreach($files as $file)
+                                <tr>
+                                    <td><a href="{{ url('file/' . $file) }}">{{ basename($file) }}</a></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                @endif
             @endif
         </div>
         <div class="col-md-6">
