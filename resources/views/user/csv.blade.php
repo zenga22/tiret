399cda232c85df3ce118f316170629d7232289a3
Roberto Guido <bob4job@gmail.com>
2017-12-22 16:35:49 +0100

    esportazione CSV utenti, aggiunti indirizzi mail

--- resources/views/user/csv.blade.php
+++ resources/views/user/csv.blade.php
@@ -1,4 +1,4 @@
-Nome,Cognome,Username
+Nome,Cognome,Username,EMail,EMail 2,EMail 3
 @foreach($users as $user)
-"{{ $user->name }}","{{ $user->surname }}","{{ $user->username }}"
+"{{ $user->name }}","{{ $user->surname }}","{{ $user->username }}","{{ $user->email }}","{{ $user->email2 }}","{{ $user->email3 }}"
 @endforeach
